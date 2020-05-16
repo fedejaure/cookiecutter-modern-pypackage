3c7a19a21240f456915f5735c37fb980537b2271
Federico Jaureguialzo <fedejaure@gmail.com>
2020-05-16 21:26:10 +0200

    Add first test

---
+++ tests/test_bake_project.py
@@ -0,0 +1,25 @@
+import datetime
+from contextlib import contextmanager
+
+from cookiecutter.utils import rmtree
+
+
+@contextmanager
+def bake_in_temp_dir(cookies, *args, **kwargs):
+    """
+    Delete the temporal directory that is created when executing the tests
+    :param cookies: pytest_cookies.Cookies,
+        cookie to be baked and its temporal files will be removed
+    """
+    result = cookies.bake(*args, **kwargs)
+    try:
+        yield result
+    finally:
+        rmtree(str(result.project))
+
+
+def test_year_compute_in_license_file(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        license_file_path = result.project.join("LICENSE")
+        now = datetime.datetime.now()
+        assert str(now.year) in license_file_path.read()
