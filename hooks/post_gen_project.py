08fc35c60b4295f4907d5c04083be31b06f922e0
Federico Jaureguialzo <fedejaure@gmail.com>
2020-05-17 00:11:30 +0200

    Add test not open source

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,12 @@
+import os
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+
+def remove_file(filepath):
+    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+if __name__ == "__main__":
+    if "Not open source" == "{{ cookiecutter.open_source_license }}":
+        remove_file("LICENSE")
--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -23,3 +23,21 @@ def test_year_compute_in_license_file(cookies):
         license_file_path = result.project.join("LICENSE")
         now = datetime.datetime.now()
         assert str(now.year) in license_file_path.read()
+
+
+def test_bake_with_defaults(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        assert result.project.isdir()
+        assert result.exit_code == 0
+        assert result.exception is None
+
+        found_toplevel_files = [f.basename for f in result.project.listdir()]
+        assert "python_boilerplate" in found_toplevel_files
+        assert "tests" in found_toplevel_files
+
+
+def test_bake_not_open_source(cookies):
+    with bake_in_temp_dir(cookies, extra_context={"open_source_license": "Not open source"}) as result:
+        found_toplevel_files = [f.basename for f in result.project.listdir()]
+        assert "LICENSE" not in found_toplevel_files
+        assert "License" not in result.project.join("README.md").read()
