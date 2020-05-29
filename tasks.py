b246e36b63c656e56b96275bbca01779a3e3c0c1
Federico Jaureguialzo <fedejaure@gmail.com>
2020-05-29 22:37:40 +0200

    Add clean tasks

---
+++ tasks.py
@@ -0,0 +1,60 @@
+"""
+Tasks for maintaining the project.
+
+Execute 'invoke --list' for guidance on using Invoke
+"""
+import platform
+from pathlib import Path
+
+from invoke import task
+from invoke.context import Context
+from invoke.runners import Result
+
+ROOT_DIR = Path(__file__).parent
+DOCS_DIR = ROOT_DIR.joinpath("docs")
+DOCS_BUILD_DIR = DOCS_DIR.joinpath("_build")
+COVERAGE_FILE = ROOT_DIR.joinpath(".coverage")
+COVERAGE_DIR = ROOT_DIR.joinpath("htmlcov")
+
+
+def _run(c: Context, command: str) -> Result:
+    return c.run(command, pty=platform.system() != "Windows")
+
+
+@task
+def clean_build(c: Context) -> None:
+    """Clean up files from package building."""
+    _run(c, "rm -fr build/")
+    _run(c, "rm -fr dist/")
+    _run(c, "rm -fr .eggs/")
+    _run(c, "find . -name '*.egg-info' -exec rm -fr {} +")
+    _run(c, "find . -name '*.egg' -exec rm -f {} +")
+
+
+@task
+def clean_python(c: Context) -> None:
+    """Clean up python file artifacts."""
+    _run(c, "find . -name '*.pyc' -exec rm -f {} +")
+    _run(c, "find . -name '*.pyo' -exec rm -f {} +")
+    _run(c, "find . -name '*~' -exec rm -f {} +")
+    _run(c, "find . -name '__pycache__' -exec rm -fr {} +")
+
+
+@task
+def clean_tests(c: Context) -> None:
+    """Clean up files from testing."""
+    _run(c, f"rm -f {COVERAGE_FILE}")
+    _run(c, f"rm -f {COVERAGE_FILE}.*")
+    _run(c, f"rm -f rm -fr {COVERAGE_DIR}")
+    _run(c, "rm -f rm -fr .pytest_cache")
+
+
+@task
+def clean_docs(c: Context) -> None:
+    """Clean up files from documentation builds."""
+    _run(c, "rm -fr {}".format(DOCS_BUILD_DIR))
+
+
+@task(pre=[clean_build, clean_python, clean_tests, clean_docs])
+def clean(c: Context) -> None:
+    """Run all clean sub-tasks."""
