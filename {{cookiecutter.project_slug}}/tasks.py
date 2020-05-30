a69ac7b8f2ac469985c1365c55943f093b36b154
Federico Jaureguialzo <fedejaure@gmail.com>
2020-05-30 13:45:52 +0200

    Change type notations until invoke support them

--- {{cookiecutter.project_slug}}/tasks.py
+++ {{cookiecutter.project_slug}}/tasks.py
@@ -21,8 +21,9 @@ def _run(c: Context, command: str) -> Result:
     return c.run(command, pty=platform.system() != "Windows")
 
 
-@task
-def clean_build(c: Context) -> None:
+@task()
+def clean_build(c):
+    # type: (Context) -> None
     """Clean up files from package building."""
     _run(c, "rm -fr build/")
     _run(c, "rm -fr dist/")
@@ -31,8 +32,9 @@ def clean_build(c: Context) -> None:
     _run(c, "find . -name '*.egg' -exec rm -f {} +")
 
 
-@task
-def clean_python(c: Context) -> None:
+@task()
+def clean_python(c):
+    # type: (Context) -> None
     """Clean up python file artifacts."""
     _run(c, "find . -name '*.pyc' -exec rm -f {} +")
     _run(c, "find . -name '*.pyo' -exec rm -f {} +")
@@ -40,21 +42,24 @@ def clean_python(c: Context) -> None:
     _run(c, "find . -name '__pycache__' -exec rm -fr {} +")
 
 
-@task
-def clean_tests(c: Context) -> None:
+@task()
+def clean_tests(c):
+    # type: (Context) -> None
     """Clean up files from testing."""
     _run(c, f"rm -f {COVERAGE_FILE}")
     _run(c, f"rm -f {COVERAGE_FILE}.*")
-    _run(c, f"rm -f rm -fr {COVERAGE_DIR}")
-    _run(c, "rm -f rm -fr .pytest_cache")
+    _run(c, f"rm -fr {COVERAGE_DIR}")
+    _run(c, "rm -fr .pytest_cache")
 
 
-@task
-def clean_docs(c: Context) -> None:
+@task()
+def clean_docs(c):
+    # type: (Context) -> None
     """Clean up files from documentation builds."""
     _run(c, "rm -fr {}".format(DOCS_BUILD_DIR))
 
 
 @task(pre=[clean_build, clean_python, clean_tests, clean_docs])
-def clean(c: Context) -> None:
+def clean(c):
+    # type: (Context) -> None
     """Run all clean sub-tasks."""
