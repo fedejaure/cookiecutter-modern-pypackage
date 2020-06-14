21aab69cff9d7ffbcda1911def1ba4e9d9e33ce9
Federico Jaureguialzo <fedejaure@gmail.com>
2020-06-14 21:39:02 +0200

    Remove pytype

--- tasks.py
+++ tasks.py
@@ -157,14 +157,6 @@ def mypy(c):
 
 
 @task()
-def pytype(c):
-    # type: (Context) -> None
-    """Run pytype."""
-    pytype_options = ["--disable=import-error"]
-    _run(c, f"poetry run pytype {' '.join(pytype_options)} {PYTHON_TARGETS_STR}")
-
-
-@task()
 def tests(c):
     # type: (Context) -> None
     """Run tests."""
