e12111af99a82baf0a1a701c263b700637059827
Federico Jaureguialzo <fedejaure@gmail.com>
2020-09-07 21:14:31 +0200

    Bump version: 0.1.3 → 0.1.4

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@ author = "Federico Jaureguialzo"
 # built documents.
 #
 # The short X.Y version.
-version = "0.1.3"
+version = "0.1.4"
 # The full version, including alpha/beta/rc tags.
-release = "0.1.3"
+release = "0.1.4"
 
 # -- General configuration ---------------------------------------------------
 
